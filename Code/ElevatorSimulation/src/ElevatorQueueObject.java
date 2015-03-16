c6d463a45a48c59c27ad969cb072c7be381d298b
Wililam <zleazor@gmail.com>
2015-03-16 21:46:38 +0100

    Completed ElevatorQueueObject with getters and setters

--- Code/ElevatorSimulation/src/ElevatorQueueObject.java
+++ Code/ElevatorSimulation/src/ElevatorQueueObject.java
@@ -9,4 +9,35 @@ public class ElevatorQueueObject {
     private Passenger p;
     private ElevatorAction action;
     private CarPosition car;
+    
+    /* Constructor */
+    public ElevatorQueueObject(Passenger p, ElevatorAction a, CarPosition c) {
+        this.p = p;
+        action = a;
+        car = c;
+    }
+    
+    /**********  Getters  ********/
+    
+    public Passenger getPassenger() {
+        return p;
+    }
+    
+    public ElevatorAction getActionType() {
+        return action;
+    }
+    
+    public CarPosition getCarPosition() {
+        return car;
+    }
+    
+    /*********** Setters *********/
+    
+    public void setCarPosition(CarPosition c) {
+        car = c;
+    }
+    
+    public void setElevatorAction(ElevatorAction a) {
+        action = a;
+    }
 }
