7a0723997d81a11a615506570455ffe324924d77
Wililam <zleazor@gmail.com>
2015-03-16 18:21:25 +0100

    Updates to the ELevatorInterface

---
+++ Code/ElevatorSimulation/src/CarPosition.java
@@ -0,0 +1,4 @@
+
+public enum CarPosition {
+	UPPER, LOWER, NULL
+}
--- Code/ElevatorSimulation/src/ElevatorInterface.java
+++ Code/ElevatorSimulation/src/ElevatorInterface.java
@@ -1,19 +1,42 @@
 
 /**
- * Possible future interfece for the elevator classes.
+ * Interface for the elevator implementing classes.
  */
 public interface ElevatorInterface {
 	/** 
-	 * Rreturn a list of passengers that disemabrks on the current floor.
+	 * Return a list of passengers that disembarks on the current floor.
+	 * Null will be returned if no passengers are leaving on this floor
+	 * or if the elevator is not currently at any floor.
 	 */
 	public Passenger[] disembarkElevator();
 
 	/**
-	 *
+	 * Return a list of passengers that embarks on the current floor,
+	 * if any. Returns null otherwise.
 	 */
 	public Passenger[] embarkElevator();
 	
 	/**
-	 * Update elevator
+	 * Update elevator with one second of travel time. Return the floor
+	 * reached, if any. If the elevator did not reach a floor, -1 will
+	 * be returned. For elevators or more than one deck, return the floor
+	 * the upper car reaches, if any.
 	 */
+	public int updateElevator();
+	
+	/**
+	 * Returns the current queue of assignments for the elevator. The
+	 * element at index 0 is handles first and so on.
+	 */
+	public ElevatorQueueObject[] getQueue();
+	
+	/**
+	 * Adds Passenger p to the queue. Assigns that p should be picked
+	 * up on index1 in the queue and dropped off at index2 in the
+	 * queue. The fact the index1 pushes index2 by one will be 
+	 * compensated for in the method. For decks with multiple positions
+	 * the CarPositions can be set to UPPER or LOWER. If the elevator
+	 * is single decked, the CarPosition is irrelevant.
+	 */
+	public void addToQueue(Passenger p, int index1, int index2, CarPosition c);
 }
