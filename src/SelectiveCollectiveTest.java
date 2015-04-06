656b838d96d895e469b9061a393b26988d5cbe81
Seymourr <jack_shabo6@hotmail.com>
2015-04-06 19:49:57 +0200

    Recent changes to SelectiveCollectiveTest(unfinished)

--- src/SelectiveCollectiveTest.java
+++ src/SelectiveCollectiveTest.java
@@ -18,13 +18,13 @@ public class SelectiveCollectiveTest extends Algorithm {
  			Passenger p = calls.removeFirst();
  			if(containsFloor(allElevators.get(0), p.getOrigin(), p.getDestination())) {
  				//Bot ride
- 				allElevators.set(0, assignLocalWithSelectiveCollective(allElevators.get(0), p));
+ 				allElevators.set(0, assignLocal(allElevators.get(0), p));
  			} else if(containsFloor(allElevators.get(1), p.getOrigin(), p.getDestination())) {
  				//Shuttle ride
  				allElevators.set(1,  assignShuttleElevator(allElevators.get(1), p)); 
  			} else if(containsFloor(allElevators.get(2), p.getOrigin(), p.getDestination())) {
  				//Top ride
- 				allElevators.set(2, assignLocalWithSelectiveCollective(allElevators.get(2), p));
+ 				allElevators.set(2, assignLocal(allElevators.get(2), p));
  			} else {
  				System.out.println("ERROR IN MANAGECALL, SIMULATION ABORTED");
  				System.exit(0);
@@ -42,7 +42,7 @@ public class SelectiveCollectiveTest extends Algorithm {
 		return getRandomElevator(elevators, zonedIndexes, p);
 	}
 
-	protected ArrayList<ElevatorInterface> assignLocalWithSelectiveCollective(ArrayList<ElevatorInterface> elevators, Passenger p) {
+	protected ArrayList<ElevatorInterface> assignLocal(ArrayList<ElevatorInterface> elevators, Passenger p) {
 		int elevatorIndex = getElevator(elevators, p);
 
 		CarPosition pos = CarPosition.NULL;
@@ -50,14 +50,16 @@ public class SelectiveCollectiveTest extends Algorithm {
 			pos = getCarPos(elevators.get(elevatorIndex), p);
 		}
 
-		int pickUp = getPoint(elevators.get(elevatorIndex), p, index start, p.getOrigin());
-		int dropOff = getPoint(elevators.get(elevatorIndex), p, pickUp. p.getDestination());
-		if(pickUp == dropOff) {
-			dropOff += 1;
+		int pickUp = getPoint(elevators.get(elevatorIndex), p, 0, p.getOrigin());
+		int dropOff = getPoint(elevators.get(elevatorIndex), p, pickUp, p.getDestination());
+		if(pickUp >= dropOff) {
+			dropOff = pickUp + 1;
 		}
 		boolean b = elevators.get(elevatorIndex).addToQueue(p, pickUp, dropOff, pos);
 		if(!b) {
 			System.out.println("FATAL ERROR in selective collective");
+			System.out.println(pickUp);
+			System.out.println(dropOff);
 			System.exit(0);
 		}
 		return elevators;
@@ -67,30 +69,55 @@ public class SelectiveCollectiveTest extends Algorithm {
 	//TODO: Fix for double decked
 	public int getPoint(ElevatorInterface e, Passenger p, int startIndex, int floor) {
 		int point = -1;
-		int pDir = (p.getDestination() - p.getOrigin())/Math.abs(p.getDestination() - p.getOrigin());
-		ArrayList<ElevatorQueueObject> iQ = e.getQueue();
+		int pAbs = Math.abs(p.getDestination() - p.getOrigin());
+		int pDir = (p.getDestination() - p.getOrigin())/pAbs;
+		LinkedList<ElevatorQueueObject> iQ = e.getQueue();
+		if(iQ.size() <= 3) {
+			return specialCase(e, p, floor, pDir);
+		}
 		int t1 = getFloor(iQ.get(startIndex + 1));
 		int t2 = getFloor(iQ.get(startIndex));
 		int preDir = (t1-t2)/Math.abs(t1-t2);
-		for(int i = startIndex + 1; i < e.getQueue().size() - 1; i++) {
-			int t1 = getFloor(e.getQueue(i + 1));
-			int t2 = getFloor(e.getQueue(i));
+		for(int i = startIndex + 1; i < iQ.size() - 1; i++) {
+			t1 = getFloor(iQ.get(i + 1));
+			t2 = getFloor(iQ.get(i));
 			int dir = (t1 - t2) / Math.abs(t1-t2);
 			if(pDir == dir) {
 				if(isBetween(t1, t2, floor)) {
-					point = i;
+					point = i + 1;
 				}
-			} else if(//Somecondition) {
-				point = i; //I + 1? Why do this?
+			} else if( (floor - getFloor(iQ.get(i))) / Math.abs(floor - getFloor(iQ.get(i))) == pDir){
+				point = i + 1; //I + 1? Why do this?
 			}
+			pDir = dir;
 		}
-		if(point = -1) {
+		if(point == -1) {
 			point = e.getQueue().size();
 		}
 		return point;
 
 	}
 
+	private int specialCase(ElevatorInterface e, Passenger p, int floor, int pDir) {
+		if(e.getQueue().size() == 0) {
+			return 0;
+		}
+		int index = -1;
+		for(int i = 0; i < e.getQueue().size(); i++) {
+			int mAbs = Math.abs((int)e.getStatus().floor - getFloor(e.getQueue().get(i)));
+			if(mAbs == 0) {
+				return 0; //Special case, for now. 
+			}
+			int dir = ((int)e.getStatus().floor - getFloor(e.getQueue().get(i)))/ mAbs;
+			if(dir == pDir && isBetween(getFloor(e.getQueue().get(i)), (int)e.getStatus().floor, floor)) {
+				index = i;
+			}
+		}
+		if(index == -1) {
+			index = e.getQueue().size();
+		}
+		return index;
+	}
 	private int getFloor(ElevatorQueueObject q) {
 		int ret = 0;
 		if(q.getActionType() == ElevatorAction.PICKUP) {
@@ -107,4 +134,5 @@ public class SelectiveCollectiveTest extends Algorithm {
 		if(l < s && (f > l && f <= s)) return true;
 		if(l == s && f == s) return true; //?
 		return false;
+	}
 }
